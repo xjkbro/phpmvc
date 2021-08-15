cbfb31695e1809db1eac4a53b8563ea4e4ab1bd3
Jason-Kyle De Lara <jason8oh8@gmail.com>
2021-08-14 18:09:09 -0700

    added views and implemented bootstrap

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application(dirname(__DIR__));
+$app->router->get('/', 'home');
+$app->router->get('/contact',  'contact');
+$app->router->post('/contact',  function () {
+    return "handling data";
+});
+$app->run();
